da74216f1b7969c3b42197ea4374d58237c71d85
Florin Godard <irgend@wasmitnetzen.de>
2018-02-13 18:50:48 +0100

    Initial commit, basic information gathering about wifi connection works

---
+++ travelMode.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+
+import os
+import subprocess
+
+ignoreAdaptors = ["lo", "docker0"]
+mobileWifiNetworks = ["WIFIonICE"]
+
+def getNetworkManagerInfo(device=""):
+	nmcli = subprocess.Popen(["/usr/bin/nmcli", "-t", "dev", "show", device], stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE, env=dict(os.environ, LANG="LC_ALL"))
+	nmcliRawInfo, errors = nmcli.communicate()
+	nmcliInfo = {}
+	if len(errors) > 0:
+		print("Errors: {}".format(errors.decode("utf-8")))
+	for line in nmcliRawInfo.decode("utf-8").rstrip().split("\n"):
+		lineContent = line.split(":", 1)
+		#print(lineContent)
+		if len(lineContent) > 2:
+			print("More than one info cell in line {}.".format(lineContent))
+			input()
+			continue
+		if len(lineContent) == 2:
+			nmcliInfo[lineContent[0]] = lineContent[1]
+		else:
+			print("Empty line {}. Setting to None.".format(lineContent[0]))
+			nmcliRawInfo[lineContent[0]] = None
+		#print("{}\n\n".format(line))
+	#print(nmcliInfo)
+	return nmcliInfo
+
+def handleWifi(adaptor,network):
+	#print("Handle wifi network on {}.".format(adaptor))
+	# check if there is a wifi network connected
+	if network['GENERAL.STATE'] == "100 (connected)":
+		networkName = network['GENERAL.CONNECTION']
+		print("Network {} is connected.".format(networkName))
+		if networkName in mobileWifiNetworks:
+			print("Computer is connected to a mobile wifi network.")
+			handleMobileWifi(adaptor,network)
+
+def handleMobileWifi(adaptor,network):
+	# disable dropbox sync
+	# disable syncthing
+
+
+def getCurrentNetwork():
+	# get all connected adaptors
+	adaptors = os.listdir('/sys/class/net/')
+	#print(adaptors)
+	for adaptor in adaptors:
+		if adaptor in ignoreAdaptors:
+			continue
+		#print("Handling network {}".format(adaptor))
+		networkInfo = getNetworkManagerInfo(adaptor)
+		# look for wireless network (wifi)
+		# TODO: also check for tethered USB connection
+		if networkInfo['GENERAL.TYPE'] == "wifi":
+			#print("Wifi network found: {}.".format(adaptor))
+			handleWifi(adaptor,networkInfo)
+
+if __name__ == '__main__':
+	getCurrentNetwork()
